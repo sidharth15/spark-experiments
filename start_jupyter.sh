d5daa0672f453e3e717dcc4a11a54f9010c40145
Sidharth Ramesh <sidharthr44@gmail.com>
2022-05-18 17:21:06 +0000

    bash script to start Jupyter notebook with public DNS of instance as parameter

---
+++ start_jupyter.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# set the environment variables for spark and python before starting jupyter - if it's not already set
+if [ -z "${SPARK_HOME}" ];
+then
+	echo "Setting environment variables for SPARK_HOME, PATH and PYTHONPATH"
+	export SPARK_HOME=/home/ec2-user/spark-3.2.1-bin-hadoop3.2
+	export PATH=$SPARK_HOME/bin:$PATH
+	export PYTHONPATH=$SPARK_HOME/python:$PYTHONPATH
+fi
+
+# this is needed when running jupyter on an ec2 instance, so that Jupyter listens on the specific public dns 
+PUBLIC_DNS=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname)
+jupyter notebook --ip=$PUBLIC_DNS
